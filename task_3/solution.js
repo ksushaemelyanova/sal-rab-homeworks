90fa168d953c7bbb0ec8043797c613959961b96c
Kseniia Emelianova <ksusha1989@yandex.ru>
2022-01-26 14:02:06 +0300

    Task 4.1

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,22 +1,7 @@
 function renderCartItem(item) {
     const product = item;
 
-    // let itemCountText;
-    // itemCountText = '';
-    // itemCountText = itemCountText + product.count;
-    // itemCountText = itemCountText + ` × `;
-    // itemCountText = itemCountText + product.price;
-    // itemCountText = itemCountText + ` ₽ = `;
-    
-    // let sum;
-    // sum = product.count * product.price;
-    // itemCountText = itemCountText + sum;
-    // itemCountText = itemCountText + ` ₽`;
-
-    
     itemCountText = ('' + product.count + ` × ` + product.price + ` ₽ = `) + (product.count * product.price + ` ₽`);
-    
-
-    
+        
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
 }
